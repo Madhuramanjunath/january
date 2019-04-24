5c1577cf8aa6bc985f07823faf3c727c6d38a217
HP <HP@DESKTOP-R4ANDCB>
2019-04-24 19:02:28 +0530

    commit1

---
+++ chromeTest/src/test/java/chromeTest/SelTest.java
@@ -0,0 +1,13 @@
+package chromeTest;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SelTest {
+	@Test
+	public void cTest(){
+		ChromeDriver driver = new ChromeDriver();
+		
+	}
+
+}
